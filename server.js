a4cf03a8cf398dc4e1d3bbb2549c5ed5f093110d
nativelocation <bluesky6460@mail.com>
2019-03-13 02:55:08 -0600

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.arguments(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/build'));
 
 app.listen(process.env.PORT || 8080);
